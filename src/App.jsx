dd4efd63f00c6fbe24c16ac1c7265b1d707dd306
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-11 14:55:22 -0400

    added sideEffects to the search

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import './App.css'
 import * as weatherService from './services/weatherService'
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import { useState, useEffect } from 'react';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+
+
 
 const App = () => {
-  const fetchData = async () => {
-    const data = await weatherService.show('Boston');
-    console.log('Data:', data);
+  const [weather, setWeather] = useState({});
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
   };
+  // The following log should be outside of the fetchData function
+
+  useEffect(() => {
+    const fetchDefaultData = async () => {
+      const data = await weatherService.show('Chicago');
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+    fetchDefaultData();
+  }, []);
 
   return (
     <main>
       <h1>Wether API</h1>
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch fetchData={fetchData} />
+      <WeatherDetails weather={weather} />
     </main>
   );
 }
